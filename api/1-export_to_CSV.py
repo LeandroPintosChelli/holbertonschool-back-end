dc689d153e8a0ca0a16870cdec114853b7115531
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-30 23:13:48 +0000

    Fix task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,30 +5,18 @@ from sys import argv
 import csv
 
 
-def to_csv():
-    """return API data"""
-    user = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    for elements in user:
-        if elements.get('id') == int(argv[1]):
-            USERNAME = elements.get('username')
-            break
+if __name__ == '__main__':
+    uid = argv[1]
+    
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url).json()
 
-    task_title = []
-    todo = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    for dict in todo:
-        if dict.get('userId') == int(argv[1]):
-            task_title.append((dict.get('completed'), dict.get('title')))
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url).json()
 
-    filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in task_title:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
-
-if __name__ == "__main__":
-    to_csv()
+    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(uid), user.get('username'),
+                                 t.get('completed'),
+                                 t.get('title')])
