4307dabf4abc41360e913814326545797965fc6a
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-30 22:33:42 +0000

    Fix task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,29 +3,29 @@
     API used: https://jsonplaceholder.typicode.com/"""
 from sys import argv
 import requests
-import json
+
 
 if __name__ == '__main__':
     todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     user = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
-    real_name = ""
-    for each_element in user:
-        if each_element.get('id') == int(argv[1]):
-            real_name = each_element.get('name')
+    name = ""
+    for elements in user:
+        if elements.get('id') == int(argv[1]):
+            name = elements.get('name')
             break
 
-    full_list = []
+    list = []
     for dict in todo:
         if dict.get('userId') == int(argv[1]):
-            full_list.append(dict)
+            list.append(dict)
 
     true_elements = []
-    for completed in full_list:
+    for completed in list:
         if completed.get('completed'):
             true_elements.append(completed)
 
     print('Employee {} is done with tasks({}/{}):'.
-          format(real_name, len(true_elements), len(full_list)))
+          format(name, len(true_elements), len(list)))
     for task in true_elements:
         print('\t {}'.format(task.get('title')))
