e679f47d3f875c465556de9b6c2a7aa16be4a345
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-01 22:39:15 +0000

    Fix task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,17 +6,16 @@ import json
 import requests
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", 'w') as JSONfile:
+    with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             u.get("id"): [{
-                "task": dict.get("title"),
-                "completed": dict.get("completed"),
-                "username": dict.get("username")
-            } for dict in requests.get(url + "todos",
-                                       params={"userId": u.get("id")}).json()]
-            for u in users}, JSONfile)
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
